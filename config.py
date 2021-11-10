758b526ebc66133a921283744b7af6074e9d4f67
imrankhan72 <imrankhan.72@gmail.com>
2021-11-10 07:25:49 +0530

    Initial commit

---
+++ config.py
@@ -0,0 +1,13 @@
+Config = {
+    'binance': {
+        "API_KEY": "tYN0zjFE32ITFyTkWK1ZkUyBSFPaEGXcka0jhqsjNLmnqeVE5XQnZP7PIwiYow0l",
+        "API_SECRET": "O1k1kXyT9CHepVQra3EcGJIMRZ8xTOxt2aH0LUX171wVnOkS5DKyQSraWfb93ff1"
+    },
+    'coindcx': {
+        "API_KEY": "e9918d4f9df887a7e1fa99b02dd01127ef93a08fc5ef61f9",
+        "API_SECRET": "78318c17a31105e04e10cbf8bb1276447cc3e4da7343bcfa393d98776da40c21"
+    },
+    'coinmarketcap': {
+        "API_KEY": "282b4f97-6612-4388-8f7b-3b7e02102d66"
+    }
+}
---
+++ main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+from binance.client import Client
+import pandas as pd
+from config import Config
+
+app = FastAPI()
+
+client = Client(Config['binance']['API_KEY'], Config['binance']['API_KEY'])
+
+
+@app.get("/")
+async def root():
+    info = client.get_exchange_info()
+
+    symbols = [x['symbol'] for x in info['symbols']]
+
+    exclude = ['UP', 'DOWN', 'BEAR', 'BULL']  # Exclude leverage tokens
+
+    non_lev = [symbol for symbol in symbols if all(excludes not in symbol for excludes in exclude)]
+
+    relevant = [symbol for symbol in non_lev if symbol.endswith('USDT')]
+
+    klines = {}
+    for symbol in relevant:
+        klines[symbol] = client.get_historical_klines(symbol, '1m', '15 mins ago utc')
+
+    returns, symbols = [], []
+
+    for symbol in relevant:
+        if len(klines[symbol]) > 0:
+            cumret = (pd.DataFrame(klines[symbol])[4].astype(float).pct_change() + 1).prod() - 1
+            returns.append(cumret)
+            symbols.append(symbol)
+
+    retdf = pd.DataFrame(returns, index=symbols, columns=['ret'])
+    top = retdf.ret.nlargest(10)
+    top_table = (top.to_frame()).to_html(classes="table")
+    return top
+
